0721a4e13dc9cd6429fc1e3bab1cf7c61387f489
forza111 <nikitos.ionkin@yandex.ru>
2021-01-19 20:47:38 +0300

    Подготовка к запуску на Heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,5 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
